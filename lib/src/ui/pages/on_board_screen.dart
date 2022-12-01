2581d46143641ce13fd4c521e9197fcd8c275de0
oakarakum <onur.ardakarakum@outlook.com>
2022-12-01 11:09:51 +0300

    on board sayfası üzerinde çalışıyorum.

--- lib/src/ui/pages/on_board_screen.dart
+++ lib/src/ui/pages/on_board_screen.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:film_critic_app/src/ui/features/widgets/Onboard_widgets/login_button.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
@@ -12,7 +13,25 @@ class OnBoardScreen extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 5.w),
         child: Column(
-          children: [],
+          children: [
+            Container(
+              height: 100,
+              width: 75,
+              decoration: BoxDecoration(
+                  color: Colors.amber,
+                  image: DecorationImage(
+                      image: AssetImage("assets/on_board_images.png"))),
+              child: Column(
+                children: [
+                  Text("THE MOVIE DE"),
+                  Text("All about movies, series, anime and much more."),
+                  Text(
+                      "Stay on top of information about movies, series, anime and more."),
+                  LoginButton()
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
