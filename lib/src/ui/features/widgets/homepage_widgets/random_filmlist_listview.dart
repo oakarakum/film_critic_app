df6f4c4f196e5ec9352961b0ec542b12a9676d84
oakarakum <onur.ardakarakum@outlook.com>
2022-12-01 14:30:58 +0300

    dio

---
+++ lib/src/ui/features/widgets/homepage_widgets/random_filmlist_listview.dart
@@ -0,0 +1,70 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:film_critic_app/src/services/homepage_services.dart';
+import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
+class RandomFilmListListview extends StatefulWidget {
+  const RandomFilmListListview({super.key});
+
+  @override
+  State<RandomFilmListListview> createState() => _RandomFilmListListviewState();
+}
+
+class _RandomFilmListListviewState extends State<RandomFilmListListview> {
+  @override
+  void initState() {
+    getTopRatedFilmsService();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 26.h,
+      width: double.infinity,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 3,
+          itemBuilder: ((context, index) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                FadeInRightBig(
+                  delay: Duration(seconds: 1),
+                  child: GestureDetector(
+                    onTap: () {
+                      /* print(value.favArtAlbumlist.items![index].id
+                          ); */
+                      /* Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => AlbumTracksScreen(
+                                    id: value.favArtAlbumlist.items![index].id
+                                        .toString(),
+                                  ))
+                                  ); */
+                    },
+                  ),
+                ),
+                SizedBox(height: 1.h),
+                FadeInUp(
+                  delay: Duration(seconds: 1),
+                  child: Padding(
+                    padding: EdgeInsets.only(right: 4.w),
+                    child: Text(
+                      "",
+                      //value.favArtAlbumlist.items![index].name.toString(),
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 2.h,
+                          color: Color(0xff3A3A3A)),
+                    ),
+                  ),
+                )
+              ],
+            );
+          })),
+    );
+  }
+}
--- lib/src/ui/pages/homepage_screen.dart
+++ lib/src/ui/pages/homepage_screen.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
 
+import 'package:film_critic_app/src/ui/features/widgets/homepage_widgets/random_filmlist_listview.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
 class HomepageScreen extends StatefulWidget {
   const HomepageScreen({super.key});
@@ -18,7 +20,12 @@ class _HomepageScreenState extends State<HomepageScreen> {
         padding: EdgeInsets.symmetric(horizontal: 20),
         child: SingleChildScrollView(
             child: Column(
-          children: [],
+          children: [
+            SizedBox(
+              height: 30.h,
+            ),
+            RandomFilmListListview()
+          ],
         )),
       ),
     );
