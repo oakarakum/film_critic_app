fc3d600d7f5e7f9cb441e692d73083e2a901704d
oakarakum <onur.ardakarakum@outlook.com>
2022-12-01 09:59:29 +0300

    developer branch oluşturuldu. Paket yüklemesi

---
+++ lib/src/ui/pages/homepage_screen.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class HomepageScreen extends StatefulWidget {
+  const HomepageScreen({super.key});
+
+  @override
+  State<HomepageScreen> createState() => _HomepageScreenState();
+}
+
+class _HomepageScreenState extends State<HomepageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: SingleChildScrollView(
+            child: Column(
+          children: [],
+        )),
+      ),
+    );
+  }
+}
---
+++ lib/src/ui/pages/on_board_screen.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class OnBoardScreen extends StatefulWidget {
+  const OnBoardScreen({super.key});
+
+  @override
+  State<OnBoardScreen> createState() => _OnBoardScreenState();
+}
+
+class _OnBoardScreenState extends State<OnBoardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
