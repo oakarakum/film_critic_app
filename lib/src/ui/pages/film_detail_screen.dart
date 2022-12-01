9fa35d704bc8c16cfe5f9445efec4cf56015fe4a
oakarakum <onur.ardakarakum@outlook.com>
2022-12-01 10:21:29 +0300

    features created

---
+++ lib/src/ui/pages/film_detail_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class FilmDetailScreen extends StatefulWidget {
+  const FilmDetailScreen({super.key});
+
+  @override
+  State<FilmDetailScreen> createState() => _FilmDetailScreenState();
+}
+
+class _FilmDetailScreenState extends State<FilmDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
