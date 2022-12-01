21f73d8e100401a6cdbb0770294684fdcbc71c5f
oakarakum <onur.ardakarakum@outlook.com>
2022-12-01 14:06:29 +0300

    homepage düzenlendi

--- lib/src/ui/pages/homepage_screen.dart
+++ lib/src/ui/pages/homepage_screen.dart
@@ -1,6 +1,5 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+// ignore_for_file: prefer_const_literals_to_create_immutables
 
-import 'package:film_critic_app/src/ui/features/widgets/homepage_widgets/search_and_profile_upbar.dart';
 import 'package:flutter/material.dart';
 
 class HomepageScreen extends StatefulWidget {
@@ -19,7 +18,7 @@ class _HomepageScreenState extends State<HomepageScreen> {
         padding: EdgeInsets.symmetric(horizontal: 20),
         child: SingleChildScrollView(
             child: Column(
-          children: [SearchandProfileUpbar()],
+          children: [],
         )),
       ),
     );
